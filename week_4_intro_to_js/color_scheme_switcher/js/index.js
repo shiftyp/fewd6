f932a4be0316a86871c3752b30f42068144df7e0
Ryan Lynch <ryan.lynch@it.com>
2014-09-03 18:13:03 -0400

    Added week 4 class 1 solution files

---
+++ week_4_intro_to_js/color_scheme_switcher/js/index.js
@@ -0,0 +1,12 @@
+document.getElementById('grayButton').onclick = switchGray;
+document.getElementById('whiteButton').onclick = switchWhite;
+
+function switchGray() {
+  document.body.style.backgroundColor = 'gray';
+ document.body.style.color = 'white';
+}
+
+function switchWhite() {
+  document.body.style.backgroundColor = 'white';
+  document.body.style.color = 'black';
+}
