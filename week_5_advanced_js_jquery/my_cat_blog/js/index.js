f5817279f60be8b62c5c6e20d40446fca3465c45
Ryan Lynch <ryan.lynch@it.com>
2014-09-10 21:29:24 -0400

    Added starter files for cat blog

---
+++ week_5_advanced_js_jquery/my_cat_blog/js/index.js
@@ -0,0 +1,19 @@
+var posts = [
+	{
+		image: 'http://placekitten.com/g/200/200',
+		title: 'More views!',
+		snippet: 'My views are up from zero to three! Hello to my loyal fans! To celebrate, here are some more cute cat pictures!'
+	}, {
+		image: 'http://placekitten.com/g/200/200',
+		title: 'Check out these cute cats!',
+		snippet: 'Wow...thats all I can say. I was surfing /cats on reddit and came across the cutest cat pictures yet! Take a look!'
+	}, {
+		image: 'http://placepu.gs/200/200',
+		title: 'Wierd cats!',
+		snippet: 'I saw these weird looking cats on the interwebs today. If you can tell me what breed they are, please do so in the comments!'
+	}, {
+		image: 'http://placekitten.com/g/200/200',
+		title: 'Cutness overload!',
+		snippet: 'OMG people, are you ready for some heavy duty cuteness? I know I am. Check out these cats! I can\'t even handle this. Too cute!'
+	}
+];
